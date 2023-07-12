ebff516c72a40ce509d61bb749acfc9f73a3323f
Cristiele <122491782+svhellen@users.noreply.github.com>
2023-07-12 20:04:00 -0300

    Game repository, service, controller

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -12,8 +12,9 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
-    public List<Game> findAll() {
+    public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
-        return result;
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
     }
 }
