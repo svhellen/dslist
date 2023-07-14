2d73573b52387c9d3e19f98d7ea730365f3a3f56
Cristiele <122491782+svhellen@users.noreply.github.com>
2023-07-14 01:51:41 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dslist.repositories;
+
+import com.devsuperior.dslist.entities.GameList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+}
---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+        List<GameList> result = gameListRepository.findAll();
+        return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+}
